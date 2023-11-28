6f31706024053e43757c51aad40d6f45892c5262
Toirov Sadi <sadi.toirov2003@gmail.com>
2023-11-28 18:30:47 +0300

    add link support to telegram bot

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
 import telebot
+from newspaper import Article
 
 from summarize.predict import summarize
 from translate.predict import translate
+
+handle_text = True
 
 print("Telegram bot is running...")
 bot = telebot.TeleBot(os.getenv("TELEGRAM_TOKEN"))
@@ -13,13 +16,27 @@ def start(message):
 
 
 @bot.message_handler(commands=['text'])
-def transfer_style(message):
+def handle_text(message):
+    global handle_text
+    handle_text = True
     bot.send_message(message.chat.id, "Please send me text, please")
 
+@bot.message_handler(commands=['link'])
+def handle_link(message):
+    global handle_text
+    handle_text = False
+    bot.send_message(message.chat.id, "Please send me the link of the website, please")
 
 @bot.message_handler(content_types=['text'])
 def get_text(message):
-    text = translate_and_summarize(message.text)
+    global handle_text
+    
+    original_text = ''
+    text = message.text
+    if not handle_text:
+        original_text, text = get_text_from_url(text)
+    
+    text = translate_and_summarize(text)
     
     response = f"Translated and summarized text: \n\n{text}"
     bot.send_message(message.chat.id, response)
@@ -28,5 +45,20 @@ def get_text(message):
 def translate_and_summarize(text: str) -> str:
     return translate(summarize(text))
 
+
+def get_text_from_url(url):
+    article = Article(url)
+    article.download()
+
+    article.parse()
+    article.nlp()
+    
+    text = article.text
+    summary = article.summary
+    
+    if len(text) > 2048:
+        text = text[:2048] + "..."
+    
+    return text, summary
 
 bot.infinity_polling()
