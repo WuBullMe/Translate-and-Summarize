301a3a78b4f0b0eaff796edc05f572e20488e381
Toirov Sadi <sadi.toirov2003@gmail.com>
2023-11-28 23:07:32 +0300

    handle large text

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
 import telebot
 from newspaper import Article
+import nltk
+from nltk import sent_tokenize
+from nltk import word_tokenize
+nltk.download('punkt', quiet=True)
 
 from summarize.predict import summarize
 from translate.predict import translate
 
 handle_text = True
+max_tokens = 300
 
 print("Telegram bot is running...")
 bot = telebot.TeleBot(os.getenv("TELEGRAM_TOKEN"))
@@ -31,12 +36,15 @@ def handle_link(message):
 def get_text(message):
     global handle_text
     
-    original_text = ''
     text = message.text
+    print("text:", text)
     if not handle_text:
-        original_text, text = get_text_from_url(text)
+        text = get_text_from_url(text)
     
-    text = translate_and_summarize(text)
+    text = split_large_text(text)
+    
+    text = [translate_and_summarize(p) for p in text]
+    text = ' '.join(text)
     
     response = f"Translated and summarized text: \n\n{text}"
     bot.send_message(message.chat.id, response)
@@ -49,16 +57,53 @@ def translate_and_summarize(text: str) -> str:
 def get_text_from_url(url):
     article = Article(url)
     article.download()
-
     article.parse()
-    article.nlp()
     
-    text = article.text
-    summary = article.summary
+    return article.text
+
+
+def split_large_text(text):
+    global max_tokens
     
-    if len(text) > 2048:
-        text = text[:2048] + "..."
+    paragraphs = []
+    num_tokens = 0
+    cur_p = []
+    tokens_array = []
+    for sent in sent_tokenize(text):
+        tokens = len(sent.split(' '))
+        if tokens == 0:
+            continue
+        
+        num_tokens += tokens
+        if num_tokens > max_tokens:
+            paragraphs.append((tokens_array, cur_p))
+            cur_p = []
+            num_tokens = tokens
+            tokens_array = []
+        
+        cur_p.append(sent)
+        tokens_array.append(num_tokens)
     
-    return text, summary
+    if len(cur_p) > 0:
+        paragraphs.append((tokens_array, cur_p))
+    
+    if len(paragraphs) > 1:
+        # balance last two paragraphs
+        while len(paragraphs[-2][1]) > 1:
+            ta1, par1 = paragraphs[-2]
+            ta2, par2 = paragraphs[-1]
+            
+            if ta2[-1] > ta1[-1]:
+                break
+            
+            par2.insert(0, par1.pop())
+            ta1.pop()
+            
+    
+    paragraphs = [' '.join(p[1]) for p in paragraphs]
+    
+    print(paragraphs)
+    return paragraphs
+
 
 bot.infinity_polling()
